e8169cdc601ebc22ef1eefb22e47ed7755c622b7
Your Name <you@example.com>
2024-07-09 11:33:16 +0100

    sunny-side

---
+++ script.js
@@ -0,0 +1,18 @@
+
+ let menuList = document.querySelector("#toggle-button");
+
+
+  const toggle = () => {
+    if(  menuList.style.display === "none" || menuList.style.display === ""  ){
+
+      menuList.style.display = "flex" 
+  } 
+  else{
+       menuList.style.display = "none"
+  }
+  
+ 
+ }
+
+
+
